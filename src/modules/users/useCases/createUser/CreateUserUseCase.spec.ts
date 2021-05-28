06d613564c0f0b58a7f236ad3c836fc7424cc6a1
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-05-28 11:17:50 -0300

    [Done] CreateUserUseCase tests

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,47 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+import { ICreateUserDTO } from "./ICreateUserDTO";
+
+let usersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Create an user", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
+  });
+
+  it("should be able to create a new user", async () => {
+    const mockUser: ICreateUserDTO = {
+      name: "Test user",
+      email: "test@user.com",
+      password: "1234"
+    }
+    const user = await createUserUseCase.execute(mockUser);
+
+    expect(user).toHaveProperty("id");
+    expect(user.name).toBe(mockUser.name);
+    expect(user.email).toBe(mockUser.email);
+    expect(user.password).not.toBe(mockUser.password);
+  });
+
+  it("should not be able to create an user with same email", async () => {
+
+    expect(async () => {
+      const mockUser: ICreateUserDTO = {
+        name: "Test user",
+        email: "test@user.com",
+        password: "1234"
+      }
+      await createUserUseCase.execute(mockUser);
+
+      const secondMockUser: ICreateUserDTO = {
+        name: "Test user 2",
+        email: "test@user.com",
+        password: "12345"
+      }
+      await createUserUseCase.execute(secondMockUser);
+    }).rejects.toBeInstanceOf(CreateUserError);
+  })
+})
